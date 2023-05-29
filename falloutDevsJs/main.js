6ebcf3604c43b85b481684fd6ad3c144ed767872
Mishmoready <moses@missionreadyhq.com>
2023-05-29 20:47:39 +1200

    .

--- falloutDevsJs/main.js
+++ falloutDevsJs/main.js
@@ -14,7 +14,57 @@ darkModeButton.addEventListener("click", function () {
   }
 });
 
-let links = document.getElementsByClassName("about-li");
-for (i = 0; i < links.length; i++) {
-  links[i].className = "link-" + i;
+const colors = [
+  "#124b00",
+  "#eb5bff",
+  "#009400",
+  "#a045be",
+  "#008700",
+  "#59006e",
+  "#dfe369",
+  "#30005d",
+  "#00ddbf",
+  "#ff0641",
+  "#00ffff",
+  "#9b0000",
+  "#00ffff",
+  "#ff513e",
+  "#00bfff",
+  "#ff6a39",
+  "#00b0ff",
+  "#c5351c",
+  "#3c43a6",
+  "#4b7000",
+  "#6b005a",
+  "#006636",
+  "#f9b6ff",
+  "#002f00",
+  "#ffc2ff",
+  "#002f04",
+  "#ff7383",
+  "#007865",
+  "#860000",
+  "#a0c7e7",
+  "#b6254a",
+  "#005143",
+  "#d77e8c",
+  "#002e74",
+  "#004748",
+  "#640030",
+  "#003a6c",
+  "#676e9a",
+  "#002a42",
+  "#625271",
+];
+const btnmash = document.getElementById("btnmash");
+const color = document.getElementsByClassName(".color");
+
+btnmash.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  mashsection.style.background = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
 }
