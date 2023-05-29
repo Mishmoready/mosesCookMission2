506ff253b1e7760a462654deb475826f335ce981
Mishmoready <moses@missionreadyhq.com>
2023-05-29 20:48:24 +1200

    .

--- falloutDevsJs/main.js
+++ falloutDevsJs/main.js
@@ -1,9 +1,11 @@
 const toggle = document.getElementsByClassName("darkModeButton");
 const body = document.querySelector("body");
+const image = document.getElementById("myImage");
 
 darkModeButton.addEventListener("click", function () {
   this.classList.toggle("bi-moon");
   if (this.classList.toggle("bi-brightness-high-fill")) {
+    image.classList.toggle;
     body.style.background = "white";
     body.style.color = "black";
     body.style.transition = "1s";
